eeb87d29c6a8367ce39cd47b71febd3f58d952ab
edksam <paakofisam.sam@gmail.com>
2021-01-18 04:12:41 +0000

    After config changes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-console */
 const logger = require("./logger");
 const app = require("./app");
-const server = process.env.PORT || app.listen(port);
 const port = process.env.PORT || app.get("port");
+const server = process.env.PORT || app.listen(port);
 
 process.on("unhandledRejection", (reason, p) =>
   logger.error("Unhandled Rejection at: Promise ", p, reason)
