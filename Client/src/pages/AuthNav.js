3c54c4db454a8292214dbbcc721e682fbe20ce14
edksam <paakofisam.sam@gmail.com>
2021-01-18 02:27:32 +0000

    After disater recovery

---
+++ Client/src/pages/AuthNav.js
@@ -0,0 +1,9 @@
+// src/components/auth-nav.js
+
+import React from "react";
+import AuthenticationButton from "./AuthenticationButton";
+import { Menu } from "antd";
+
+const AuthNav = () => <AuthenticationButton />;
+
+export default AuthNav;
